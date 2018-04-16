6c4aa1b3b2b71aa26567e82163ee56e42df2f1d7
Nigel Jena <hypej.10@gmail.com>
2018-04-16 14:28:59 +0000

    fix users

---
+++ db/migrate/20180416142209_add_confirmed_at_to_users.rb
@@ -0,0 +1,4 @@
+class AddConfirmedAtToUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324235832) do
+ActiveRecord::Schema.define(version: 20180416142209) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
