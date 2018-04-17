339b1d301b89c4079da64570a572e39b69bea5cc
samo <hypej.10@gmail.com>
2018-04-17 11:23:10 +0200

    fix prodution user signin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.action_mailer.default_url_options = { :host => 'www.vanza.co.za' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => 'vanzabzt@vanza.co.za',
