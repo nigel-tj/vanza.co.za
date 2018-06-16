66bb4464f878b11dee1333b53a30471696d8e654
Nigel Jena <hypej.10@gmail.com>
2018-06-16 13:47:12 +0000

    create table sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422174246) do
+ActiveRecord::Schema.define(version: 20180616134116) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 20180422174246) do
     t.integer  "artist_id"
     t.string   "artist_name"
   end
+
+# Could not dump table "artists" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "dowload_logs", force: :cascade do |t|
     t.integer  "download_id"
@@ -170,6 +173,16 @@ ActiveRecord::Schema.define(version: 20180422174246) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "standard_tickets", force: :cascade do |t|
     t.integer  "user_id"
